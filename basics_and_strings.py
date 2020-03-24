c7ff269ce997bf3d6736733819ee568fead7c77c
SagarKurada <59098038+SagarKurada@users.noreply.github.com>
2020-03-24 08:45:06 -0400

    Update basics_and_strings.py

--- basics_and_strings.py
+++ basics_and_strings.py
@@ -389,6 +389,22 @@ ii)
         
  58. Printing the alphabets:
     capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+    
+ 59. BFS:
+    row, col = len(grid), len(grid[0])
+        rotting = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 2}
+        fresh = {(i, j) for i in range(row) for j in range(col) if grid[i][j] == 1}
+        timer = 0
+        while fresh:
+            if not rotting: return -1
+            rotting = {(i+di, j+dj) for i, j in rotting for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)] if (i+di, j+dj) in fresh}
+            fresh -= rotting
+            timer += 1
+        return timer
+    
+    
+    
+    
  Data Frames:
     1. columns data type: data.dtypes
     2. String to datetime : tx_data['InvoiceDate'] = pd.to_datetime(tx_data['InvoiceDate'])
